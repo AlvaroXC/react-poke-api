fbca19bcaa415276d66e028331478371fad2c711
Alvaro Xool <alvaroxool14@gmail.com>
2026-03-13 20:07:34 -0600

    feat(hooks): extract data fetching logic into usePokemons custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useSelector } from "react-redux"
+import { usePokemons } from "./hooks/usePokemons"
+
 function App() {
+
+  usePokemons()
+
+  const { pokemons, isLoading, error } = useSelector( state => state.pokemon)
 
   return (
     <>
+      {isLoading && <p>Cargando Pokémon...</p>}
+      {error && <p className="text-poke-red">{error}</p>}
+      
+      {!isLoading && !error && (
+        <p>Se cargaron {pokemons.length} Pokémon exitosamente</p>
+      )}
       
     </>
   )
